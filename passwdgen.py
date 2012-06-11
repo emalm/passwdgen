490a04c56a2c3f2823944f355be03b3e72146a06
Eric Malm <eric.malm@gmail.com>
2012-06-11 11:17:14 -0400

    Changing argument processing to argparse module; adding support for different keyboard weights.

--- passwdgen.py
+++ passwdgen.py
@@ -1,8 +1,11 @@
 #! /usr/bin/env python
-import random, sys, getopt
+import random, sys
+import getopt
+import argparse
 from math import log
 
 versionstring = "Password generator script, version 0.05."
+version = '0.06'
 
 def buildusage(name):
 	return 'Usage: ' + name + """ [-hvb] [-t <template>] [-n <number>]
@@ -28,34 +31,30 @@ Password templates use the following characters:
 def main(argv = None):
 	if argv is None:
 		argv = sys.argv
-	try:
-		(opts, args) = getopt.getopt(argv[1:], "hvbt:n:", ["help", "version"])
-	except getopt.GetoptError, err:
-		print str(err) # will print something like "option -a not recognized"
-		print usage
-		return 2
 
+	parser = argparse.ArgumentParser(description='Password generator script.')
+
+	parser.add_argument('-t', '--template', default='cvcpcvccvcpdd')
+	parser.add_argument('-n', '--num', type=int, default=10)
+	parser.add_argument('-b', '--bits', action='store_true')
+
+	parser.add_argument('-k', '--keyboard', 
+		choices=['uniform', 'u', 'qwerty', 'q', 'dvorak', 'd'],
+		default='uniform')
+		
+	parser.add_argument('-v', '--version', action='version', 
+						version='%(prog)s' + version)
+						
+	args = parser.parse_args(argv[1:])
+	# print args
+	
+	bitsonly = args.bits
 	patterns = []
-	bitsonly = False
-	numtogen = 10
+	patterns.append(args.template)
+	numtogen = args.num
+	keyboard = args.keyboard
 
-	for (o, a) in opts:
-		if o in ("-h", "--help"):
-			print buildusage(argv[0])
-			return 0
-		elif o in ("-v", "--version"):
-			print versionstring
-			return 0
-		elif o == "-t":
-			# should check this is a valid pattern
-			patterns.append(a)
-		elif o == "-n":
-			# should check this is a valid integer
-			numtogen = int(a)
-		elif o == "-b":
-			bitsonly = True
-
-	pools = make_pools()
+	pools = make_pools(keyboard)
 
 	if len(patterns) == 0:
 		patterns.append("cvcpcvccvcpdd")
@@ -79,26 +78,46 @@ def make_passwd(pattern, pools):
 	return passwd
 
 
-def make_pools():
+def make_pools(keyboard):
+	consonants = {
+		'u': 'bcdfghjklmnpqrstvwxz',
+		'd': 'dhtns' * 4 + 'pfgcrl' * 2 + 'qjkxbmwvz',
+		'q': 'sdfghjkl' * 4 + 'qwrtp' * 2 + 'zxcvbnm'
+	}
+	
+	consonants['uniform'] = consonants['u']
+	consonants['dvorak'] = consonants['d']
+	consonants['qwerty'] = consonants['q']
+	
+	vowels = {
+		'u': 'aeiouy',
+		'd': 'aeiou' * 3 + 'y' * 2,
+		'q': 'a' * 3 + 'eyuio' * 2
+	}
+	
+	vowels['uniform'] = vowels['u']
+	vowels['dvorak'] = vowels['d']
+	vowels['qwerty'] = vowels['q']
+	
 	pools = {}
 	
-	pools['c'] = list('bcdfghklmnprstvwxz')
-	pools['C'] = pools["c"] + [ch.upper() for ch in pools["c"]]
+	pools['c'] = list(consonants[keyboard])
+	pools['C'] = pools['c'] + [ch.upper() for ch in pools['c']]
 	
-	pools['v'] = list('aeiouy')
-	pools['V'] = pools["v"] + [ch.upper() for ch in pools["v"]]
+	pools['v'] = list(vowels[keyboard])
+	pools['V'] = pools['v'] + [ch.upper() for ch in pools['v']]
 	
-	pools['l'] = pools["c"] + pools["v"]
-	pools['L'] = pools["l"] + [ch.upper() for ch in pools["l"]]
+	pools['l'] = pools['c'] + pools['v']
+	pools['L'] = pools['l'] + [ch.upper() for ch in pools['l']]
 	
 	pools['d'] = [str(digit) for digit in range(10)]
-	pools['D'] = pools["d"]
+	pools['D'] = pools['d']
 	
 	pools['p'] = list('!@#%^&()-_+=[]{};,.?')
-	pools['P'] = pools["p"]
+	pools['P'] = pools['p']
 	
-	pools['h'] = pools["d"] + list('abcdef')
-	pools['H'] = pools["d"] + list('ABCDEF')
+	pools['h'] = pools['d'] + list('abcdef')
+	pools['H'] = pools['d'] + list('ABCDEF')
 	
 	return pools
 	
@@ -118,6 +137,7 @@ def array_entropy(seq):
 		
 	# convert to base-2 logs
 	bits /= log(2)
+	
 	return bits
 	
 def pattern_entropy(pattern, pools):
